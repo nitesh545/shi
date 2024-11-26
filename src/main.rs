b50bcd59497f00484a89c6626133b0de25f18d14
nitecse <hitman12457868@gmail.com>
2024-11-27 03:55:30 +0530

    main file added

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+use bevy::app::AppExit;
+use bevy::core_pipeline::bloom::BloomSettings;
+use bevy::ecs::system::SystemParam;
 use bevy::input::mouse::{MouseMotion, MouseWheel};
 use bevy::math::VectorSpace;
 use bevy::prelude::*;
+use bevy::window::{CursorGrabMode, PrimaryWindow};
+use bevy_rapier3d::prelude::*;
 use rand::{thread_rng, Rng};
 
-const ENEMY_SPEED: f32 = 5.0;
-const ENEMY_SHOOTING_DISTANCE: f32 = 15.0;
+const ENEMY_SPEED: f32 = 35.0;
+const ENEMY_SHOOTING_DISTANCE: f32 = 40.0;
 const SPAWN_DURATION: f32 = 2.0;
 const PROJECTILE_SPEED: f32 = 50.0;
 
@@ -27,6 +32,9 @@ struct MainCamera;
 #[derive(Component)]
 struct Rock;
 
+#[derive(Component)]
+struct Collectible;
+
 #[derive(Resource)]
 struct EnemySpawnTimer(Timer);
 
@@ -40,6 +48,8 @@ fn main() {
             TimerMode::Repeating,
         )))
         .add_plugins(DefaultPlugins)
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_plugins(RapierDebugRenderPlugin::default())
         .add_plugins(PluginInit)
         .run();
 }
@@ -52,35 +62,64 @@ impl Plugin for PluginInit {
             5.0,
             TimerMode::Repeating,
         )))
+        .insert_resource(ClearColor(Color::BLACK))
         .insert_resource(EnemyProjectileSpawnTimer(Timer::from_seconds(
             2.0,
             TimerMode::Repeating,
         )))
         .add_systems(Startup, setup)
+        .add_systems(Update, debug_inputs)
         .add_systems(Update, player_movement)
         .add_systems(Update, adjust_player_speed)
         .add_systems(Update, adjust_camera_distance)
         .add_systems(Update, player_rotation)
-        .add_systems(Update, enemy_spawner)
-        .add_systems(Update, enemy_shooting)
-        .add_systems(Update, enemy_projectile_movement)
-        .add_systems(Update, enemy_movement);
+        // .add_systems(Update, enemy_spawner)
+        // .add_systems(Update, enemy_shooting)
+        // .add_systems(Update, enemy_projectile_movement)
+        // .add_systems(Update, enemy_movement)
+        ;
+    }
+}
+
+fn debug_inputs(
+    mut q_windows: Query<&mut Window, With<PrimaryWindow>>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut exit: EventWriter<AppExit>,
+) {
+    let mut primary_window = q_windows.single_mut();
+    if keyboard_input.pressed(KeyCode::F1) {
+        primary_window.cursor.grab_mode = CursorGrabMode::Locked;
+        primary_window.cursor.visible = false;
+    }
+    if keyboard_input.pressed(KeyCode::F2) {
+        primary_window.cursor.grab_mode = CursorGrabMode::None;
+        primary_window.cursor.visible = true;
+    }
+    if keyboard_input.pressed(KeyCode::Escape) {
+        exit.send(AppExit::Success);
     }
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let camera = commands
-        .spawn(Camera3dBundle {
-            transform: Transform::from_xyz(0.0, 50.0, 0.0).looking_at(Vec3::ZERO, Vec3::NEG_Y),
-            ..default()
-        })
+        .spawn((
+            Camera3dBundle {
+                transform: Transform::from_xyz(0.0, 50.0, 0.0).looking_at(Vec3::ZERO, Vec3::NEG_Y),
+                camera: Camera {
+                    hdr: true,
+                    ..default()
+                },
+                ..default()
+            },
+            BloomSettings::OLD_SCHOOL,
+        ))
         .insert(MainCamera)
         .id();
 
     commands.spawn(DirectionalLightBundle {
         directional_light: DirectionalLight {
             shadows_enabled: false,
-            illuminance: 50000.0,
+            illuminance: 5000.0,
             ..default()
         },
         transform: Transform::from_xyz(0.0, 1000.0, 0.0).looking_at(Vec3::ZERO, Vec3::NEG_Y),
@@ -89,35 +128,92 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 
     commands.spawn(
         (SceneBundle {
-            scene: asset_server.load("H:/Dev/Shi/assets/spacewithmeteors.glb#Scene0"),
+            scene: asset_server.load("H:/Dev/Shi/assets/spacewithmeteorswhite.glb#Scene0"),
             ..default()
         }),
     );
 
+    let thruster = commands
+        .spawn(
+            (SpatialBundle::from_transform(
+                Transform::from_xyz(10.0, 0.0, 0.0).with_scale(Vec3::splat(0.25)),
+            )),
+        )
+        .insert(Collectible)
+        .insert(RigidBody::KinematicPositionBased)
+        .insert(Collider::cuboid(5.0, 5.0, 5.0))
+        .id();
+
+    // commands.spawn(
+    //     (SceneBundle {
+    //         scene: asset_server.load("H:/Dev/Shi/assets/star.glb#Scene0"),
+    //         transform: Transform::from_xyz(-10000.0, -25000.0, 0.0).with_scale(Vec3::splat(1000.0)),
+    //         ..default()
+    //     }),
+    // );
+
+    // commands.spawn(
+    //     (SceneBundle {
+    //         scene: asset_server.load("H:/Dev/Shi/assets/smallstars.glb#Scene0"),
+    //         transform: Transform::from_xyz(0.0, -29000.0, 0.0).with_scale(Vec3::splat(10.0)),
+    //         ..default()
+    //     }),
+    // );
+
     for _i in 0..200 {
         commands.spawn((
             SceneBundle {
-                scene: asset_server.load("H:/Dev/Shi/assets/meteorite.glb#Scene0"),
+                scene: asset_server.load("H:/Dev/Shi/assets/aura_meteor.glb#Scene0"),
                 transform: Transform::from_scale(Vec3::splat(thread_rng().gen_range(20.0..100.0)))
                     .with_translation(Vec3 {
                         x: thread_rng().gen_range(-2500.0..2500.0),
                         y: thread_rng().gen_range(-1000.0..0.0),
                         z: thread_rng().gen_range(-2500.0..2500.0),
-                    }),
+                    })
+                    .with_rotation(Quat::from_euler(
+                        EulerRot::XYZ,
+                        thread_rng().gen_range(0.0..359.0),
+                        thread_rng().gen_range(0.0..359.0),
+                        thread_rng().gen_range(0.0..359.0),
+                    )),
                 ..default()
             },
             Rock,
         ));
     }
 
-    let player_scene_handle: Handle<Scene> = asset_server.load("PlayerSpaceShip.glb#Scene0");
+    let player_scene_handle: Handle<Scene> = asset_server.load("SpaceShipParts/core.glb#Scene0");
+    // commands
+    //     .spawn((
+    //         SceneBundle {
+    //             scene: player_scene_handle,
+    //             transform: Transform::from_xyz(0.0, 0.0, 0.0).with_scale(Vec3::splat(0.25)),
+    //             ..default()
+    //         },
+    //         RigidBody::KinematicPositionBased,
+    //         Collider::capsule_x(9.0, 2.5),
+    //         Player { speed: 5.0 },
+    //     ))
+    //     .add_child(thruster)
+    //     .add_child(camera);
+
     commands
-        .spawn(SceneBundle {
-            scene: player_scene_handle,
-            transform: Transform::from_xyz(0.0, 0.0, 0.0).with_scale(Vec3::splat(0.25)),
-            ..default()
+        .spawn((
+            SpatialBundle::from_transform(
+                Transform::from_xyz(0.0, 0.0, 0.0).with_scale(Vec3::splat(0.25)),
+            ),
+            RigidBody::KinematicPositionBased,
+            Collider::capsule_x(9.0, 2.5),
+            Player { speed: 5.0 },
+        ))
+        .with_children(|parent| {
+            parent.spawn(SceneBundle {
+                scene: player_scene_handle,
+                transform: Transform::from_xyz(0.0, 0.0, 0.0), //relative to parent
+                ..default()
+            });
         })
-        .insert(Player { speed: 5.0 })
+        .add_child(thruster)
         .add_child(camera);
 }
 
@@ -182,7 +278,7 @@ fn player_rotation(
     }
 
     // Sensitivity can be adjusted to your liking
-    let sensitivity = Vec2::new(0.0025, 0.0025);
+    let sensitivity = Vec2::new(0.0005, 0.0005);
 
     for mut transform in query.iter_mut() {
         transform.rotate(Quat::from_rotation_y(-delta.x * sensitivity.x));
@@ -197,7 +293,7 @@ fn enemy_spawner(
 ) {
     if timer.0.tick(time.delta()).just_finished() {
         let mut rng = rand::thread_rng();
-        let enemy_ship_scene_handle: Handle<Scene> = asset_server.load("enemyship.glb#Scene0");
+        let enemy_ship_scene_handle: Handle<Scene> = asset_server.load("enemyship2.glb#Scene0");
         commands
             .spawn(SceneBundle {
                 scene: enemy_ship_scene_handle,
@@ -258,7 +354,12 @@ fn enemy_shooting(
                     let direction =
                         (player_transform.translation - enemy_transform.translation).normalize();
                     let rotation = Quat::from_rotation_arc(Vec3::NEG_Z, direction);
-                    let missile_handle: Handle<Scene> = asset_server.load("missile.glb#Scene0");
+                    let scale = Vec3 {
+                        x: 0.5,
+                        y: 0.5,
+                        z: 0.5,
+                    };
+                    let missile_handle: Handle<Scene> = asset_server.load("missile1.glb#Scene0");
 
                     commands.spawn((
                         SceneBundle {
@@ -266,6 +367,7 @@ fn enemy_shooting(
                             transform: Transform {
                                 translation: enemy_transform.translation,
                                 rotation: rotation,
+                                scale: scale,
                                 ..default()
                             },
                             ..default()
@@ -291,4 +393,10 @@ fn enemy_projectile_movement(
             commands.entity(entity).despawn_recursive();
         }
     }
+}
+
+fn collectible_collision_detection(
+    mut query1: Query<&Transform, With<Player>>,
+    mut query2: Query<&Transform, With<Collectible>>,
+) {
 }
